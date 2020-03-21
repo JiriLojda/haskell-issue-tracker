2ed01b86061b2b13013505b2a244362ece84dc8f
unknown <lojdaj@seznam.cz>
2020-03-21 18:57:39 +0100

    Add option to delete and rename projects

--- src/Handler/Project.hs
+++ src/Handler/Project.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Handler.Project where
 
 import Import hiding (putStrLn, (.))
 import Data.UUID.V4
 import Data.UUID
+import Data.Text
+import Control.Monad.Trans.Maybe
 import Model
 
 postNewProjectR :: Handler Value
@@ -23,3 +27,26 @@ getProjectR pId = do
     case project of
         Just x -> returnJson x
         Nothing -> notFound
+
+deleteProjectR :: ProjectId -> Handler Value
+deleteProjectR pId = do
+    project <- runDB $ delete pId
+    sendStatusJSON ok200 ()
+
+data RenameProjectPayload = RenameProjectPayload { name :: Text } deriving (Generic, Show)
+
+instance FromJSON RenameProjectPayload where
+
+patchRenameProjectR :: ProjectId -> Handler Value
+patchRenameProjectR pId = do
+    payload <- (requireJsonBody :: Handler RenameProjectPayload)
+    updatedProject <- runDB $ runMaybeT $ renameProject pId $ name payload
+    case updatedProject of
+        Just x -> returnJson x
+        Nothing -> notFound
+
+renameProject :: ProjectId -> Text -> MaybeT (YesodDB App) (Entity Project)
+renameProject pId newName = do
+        originalProject <- MaybeT $ get $ pId
+        updatedProject <- lift $ updateGet pId [ ProjectName =. newName ]
+        return $ Entity pId updatedProject
