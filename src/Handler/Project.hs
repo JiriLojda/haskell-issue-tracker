e9f706d814a787f0619dce1c5008950f2f8f62a2
unknown <lojdaj@seznam.cz>
2020-03-21 19:53:19 +0100

    Enable archiving and unarchiving projects

--- src/Handler/Project.hs
+++ src/Handler/Project.hs
@@ -39,13 +39,36 @@ instance FromJSON RenameProjectPayload where
 patchRenameProjectR :: ProjectId -> Handler Value
 patchRenameProjectR pId = do
     payload <- (requireJsonBody :: Handler RenameProjectPayload)
-    updatedProject <- runDB $ runMaybeT $ renameProject pId $ name payload
+    updatedProject <- renameProject pId $ name payload
     case updatedProject of
         Just x -> returnJson x
         Nothing -> notFound
 
-renameProject :: ProjectId -> Text -> MaybeT (YesodDB App) (Entity Project)
-renameProject pId newName = do
-        _ <- MaybeT $ get $ pId
-        updatedProject <- lift $ updateGet pId [ ProjectName =. newName ]
-        return $ Entity pId updatedProject
+renameProject :: ProjectId -> Text -> Handler (Maybe (Entity Project))
+renameProject pId newName = runDB $ runMaybeT $ updateProject pId [ ProjectName =. newName ]
+
+archiveProject :: ProjectId -> Handler (Maybe (Entity Project))
+archiveProject pId = runDB $ runMaybeT $ updateProject pId [ ProjectIsArchived =. True ]
+
+unarchiveProject :: ProjectId -> Handler (Maybe (Entity Project))
+unarchiveProject pId = runDB $ runMaybeT $ updateProject pId [ ProjectIsArchived =. False ]
+
+updateProject :: ProjectId -> [Update Project] -> MaybeT (YesodDB App) (Entity Project)
+updateProject pId mods = do
+    _ <- MaybeT $ get $ pId
+    updatedProject <- lift $ updateGet pId mods
+    return $ Entity pId updatedProject
+
+patchArchiveProjectR :: ProjectId -> Handler Value
+patchArchiveProjectR pId = do
+    updatedProject <- archiveProject pId
+    case updatedProject of
+        Just x -> returnJson x
+        Nothing -> notFound
+
+patchUnarchiveProjectR :: ProjectId -> Handler Value
+patchUnarchiveProjectR pId = do
+    updatedProject <- unarchiveProject pId
+    case updatedProject of
+        Just x -> returnJson x
+        Nothing -> notFound
